71cbb1deb98b89b3b11ab1a8f10ebd395bf6348d
MiguelDev1 <loormiguel40@gmail.com>
2026-06-20 17:07:07 -0500

    feat: regenerar la carpeta android para el APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Trajectory-Front',
+  appId: 'com.trajectory.app',
+  appName: 'Trajectory',
   webDir: 'www'
 };
 
